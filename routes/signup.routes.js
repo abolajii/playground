4cad61d48b17b1bdbec72eb26f79075f3ed92b06
devaj-cmd <therealdevaj@gmail.com>
2023-03-15 18:32:14 +0100

    add picture functionality

--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination(req, file, callback) {
-    callback(null, "./tmp");
+    callback(null, "../../../../../../tmp");
   },
   filename(req, file, callback) {
     callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
