9e34bbb90be52b926630915d875c280e62421ee8
devaj-cmd <therealdevaj@gmail.com>
2023-03-15 06:10:43 +0100

    second cyclic

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5009;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json({ msg: "Hello from cyclic" });
+  res.status(200).send("Hello from cyclic");
 });
 
 app.listen(PORT, console.log(`Listening on ${PORT}`));
