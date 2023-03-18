40ebcb51afd5999fda12a5027fcee807ec6a1a66
devaj-cmd <therealdevaj@gmail.com>
2023-03-18 11:31:36 +0100

    cors

--- routes/images.routes.js
+++ routes/images.routes.js
@@ -3,7 +3,7 @@ const { downloadFile } = require("../utils/s3");
 const imageRouter = require("express").Router();
 
 imageRouter.get("/images/:key", (req, res) => {
-  console.log("get image");
+  console.log("get image", key);
   const key = req.params.key;
   const readStream = downloadFile(key);
   readStream.pipe(res);
