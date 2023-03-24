dac177149aabe6af90115e0dce9fa31e3cee275f
devaj-cmd <therealdevaj@gmail.com>
2023-03-24 19:00:43 +0100

    signin other services

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 6000;
 
-// var whitelist = [
-//   "http://localhost:3000",
-//   "http://localhost:3000/details",
-//   "https://lovebirdz-759c8.web.app",
-//   "https://lovebirdz-759c8.web.app/details",
-// ];
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
+var whitelist = [
+  "http://localhost:3000",
+  "http://localhost:3000/details",
+  "https://lovebirdz-759c8.web.app",
+  "https://lovebirdz-759c8.web.app/details",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(cors());
+// app.use(cors());
 
 const mongoose = require("mongoose");
 const signInRoute = require("./routes/signin.routes");
