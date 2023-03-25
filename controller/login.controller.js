1b35d33487fe9f941571662a95946bd86723092b
devaj-cmd <therealdevaj@gmail.com>
2023-03-25 15:20:57 +0100

    forgot password

--- controller/login.controller.js
+++ controller/login.controller.js
@@ -592,6 +592,7 @@ const editProfile = (req, res) => {
 
         res.status(200).json({
           message: "User updated successfully",
+          user: userWithoutPassword,
         });
       } else {
         res.status(404).json({ message: "User not found" });
