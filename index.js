14ad1957ae213a607ca4acddf459551962f7908f
devaj-cmd <therealdevaj@gmail.com>
2023-03-29 13:21:37 +0100

    update

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 const express = require("express");
 const app = express();
-const db = require("./model");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 6000;
 
-// var whitelist = [
-//   "http://localhost:3000",
-//   "http://localhost:3000/details",
-//   "https://lovebirdz-759c8.web.app",
-//   "https://lovebirdz-759c8.web.app/details",
-// ];
-// var corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
-// };
+var whitelist = [
+  "http://localhost:3000",
+  "http://localhost:3000/details",
+  "https://lovebirdz-759c8.web.app",
+  "https://lovebirdz-759c8.web.app/details",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-app.use(cors());
+// app.use(cors());
 
 const mongoose = require("mongoose");
 
